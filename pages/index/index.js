27f2647a8cae6bbc943453787d71e5e9ab0dd22b
lvdream <lvdream@gmail.com>
2018-08-27 23:31:27 +0800

    init index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,62 @@
 import Page from '../../common/page';
-
+const app = getApp();
+var constant = app.globalData;
 Page({
   data: {
-    active: 0,
-    active2: 0,
-    icon: {
-      normal:
-        'https://img.yzcdn.cn/public_files/2017/10/13/c547715be149dd3faa817e4a948b40c4.png',
-      active:
-        'https://img.yzcdn.cn/public_files/2017/10/13/793c77793db8641c4c325b7f25bf130d.png'
-    }
+    active: 0
   },
-
+  uploadPhoto() {
+    var that = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths;
+        upload(that, tempFilePaths);
+      }
+    })
+  },
   onChange(event) {
-    console.log(event.detail);
+    console.log(event);
   }
 });
+function upload(page, path) {
+  wx.showToast({
+    icon: "loading",
+    title: "正在上传"
+  }),
+    wx.uploadFile({
+      url: constant.SERVER_URL + "/upload",
+      filePath: path[0],
+      name: 'file',
+      header: { "Content-Type": "multipart/form-data" },
+      formData: {
+        //和服务器约定的token, 一般也可以放在header中
+        'session_token': wx.getStorageSync('session_token')
+      },
+      success: function (res) {
+        if (res.statusCode != 200) {
+          wx.showModal({
+            title: '提示',
+            content: '上传失败',
+            showCancel: false
+          })
+          return;
+        }
+        var data = res.data
+        
+      },
+      fail: function (e) {
+        wx.showModal({
+          title: '提示',
+          content: '上传失败',
+          showCancel: false
+        })
+      },
+      complete: function () {
+        wx.hideToast();  //隐藏Toast
+      }
+    })
+}
