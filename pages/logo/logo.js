3c138b1666a05dfd369274dd6c99fb562e3f12d7
lvdream <lvdream@gmail.com>
2018-09-20 21:05:52 +0800

    首页加载

--- pages/logo/logo.js
+++ pages/logo/logo.js
@@ -2,5 +2,16 @@
 const util = require('../../utils/util.js')
 
 Page({
-  
+  onLoad:function(e){
+    let second = 2;
+    const timer = setInterval(() => {
+      second--;
+      if (!second) {
+        clearInterval(timer);
+        wx.redirectTo({
+          url: '../index/index'
+        })
+      }
+    }, 1000);
+  }
 })
